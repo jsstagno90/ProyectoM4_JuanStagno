734f417a821db7b1862923f293c4e19cd70e8c25
Juan Stagno <jsstagno@hotmail.com>
2026-07-27 08:54:06 -0300

    Empezamos con tasks

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,5 +1,61 @@
+import { useState } from "react";
+import { createTask } from "../services/taskService";
+
 function Tasks() {
-  return <h1>Tasks</h1>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      await createTask({
+        title,
+        description,
+      });
+
+      alert("Tarea creada correctamente");
+
+      setTitle("");
+      setDescription("");
+    } catch (error) {
+      console.error(error);
+      
+    }
+  }
+
+  return (
+    <div>
+      <h1>Mis tareas</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Título</label>
+          <br />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <div>
+          <label>Descripción</label>
+          <br />
+          <textarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+
+        <br />
+
+        <button type="submit">Crear tarea</button>
+      </form>
+    </div>
+  );
 }
 
 export default Tasks;
--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,6 +1,14 @@
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  getDocs,
+  query,
+  where,
+  orderBy,
+} from "firebase/firestore";
 import { auth, db } from "./firebase";
-import type { NewTask } from "../types/task";
+import type { NewTask, Task } from "../types/task";
 
 
 export async function createTask(newTask: NewTask) {
@@ -9,5 +17,33 @@ export async function createTask(newTask: NewTask) {
   if (!user) {
     throw new Error("No hay un usuario autenticado.");
   }
+  await addDoc(collection(db, "tasks"), {
+    title: newTask.title,
+    description: newTask.description,
+    completed: false,
+    userId: user.uid,
+    createdAt: serverTimestamp(),
+  });
 
+}
+
+export async function getTasks(): Promise<Task[]> {
+  const user = auth.currentUser;
+
+  if (!user) {
+    throw new Error("No hay un usuario autenticado.");
+  }
+
+  const q = query(
+    collection(db, "tasks"),
+    where("userId", "==", user.uid),
+    orderBy("createdAt", "desc")
+  );
+
+  const snapshot = await getDocs(q);
+
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...(doc.data() as Omit<Task, "id">),
+  }));
 }
