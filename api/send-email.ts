e11ffd4dd1f1b9f158af510fe2d130a89fbf0d63
Juan Stagno <jsstagno@hotmail.com>
2026-07-28 21:20:21 -0300

    Configurar AWS SES

--- api/send-email.ts
+++ api/send-email.ts
@@ -2,53 +2,67 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 const ses = new SESClient({
-  region: process.env.AWS_REGION,
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
 });
 
 export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
+    req: VercelRequest,
+    res: VercelResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Método no permitido" });
-  }
+    if (req.method !== "POST") {
+        return res.status(405).json({ message: "Método no permitido" });
+    }
 
-  const { to, subject, message } = req.body;
 
-  try {
-    const command = new SendEmailCommand({
-      Source: process.env.SES_FROM_EMAIL!,
-      Destination: {
-        ToAddresses: [to],
-      },
-      Message: {
-        Subject: {
-          Data: subject,
-        },
-        Body: {
-          Text: {
-            Data: message,
-          },
-        },
-      },
-    });
+    const { to, subject, message } = req.body;
 
-    await ses.send(command);
+    if (!to || !subject || !message) {
+        return res.status(400).json({
+            success: false,
+            message: "Faltan datos para enviar el correo.",
+        });
+    }
+    try {
+        const command = new SendEmailCommand({
+            Source: process.env.SES_FROM_EMAIL!,
+            Destination: {
+                ToAddresses: [to],
+            },
+            Message: {
+                Subject: {
+                    Data: subject,
+                },
+                Body: {
+                    Text: {
+                        Data: message,
+                    },
+                },
+            },
+        });
 
-    return res.status(200).json({
-      success: true,
-      message: "Correo enviado correctamente",
-    });
-  } catch (error) {
-    console.error(error);
+        await ses.send(command);
 
-    return res.status(500).json({
-      success: false,
-      message: "Error al enviar el correo",
-    });
-  }
+        return res.status(200).json({
+            success: true,
+            message: "Correo enviado correctamente",
+        });
+    } catch (error) {
+        console.error(error);
+
+        return res.status(500).json({
+            success: false,
+            message: "Error al enviar el correo",
+        });
+    }
 }
+
+
+export const config = {
+    api: {
+        bodyParser: true,
+    },
+};
